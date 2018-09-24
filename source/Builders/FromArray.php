3501f3cfbb5556013d440c75fb4e6332343f0af8
Maike <maike_jbe@hotmail.com>
2018-09-24 15:25:03 -0300

    Corrigido instanciamento de objetos e adicionadas verificações

--- source/Builders/FromArray.php
+++ source/Builders/FromArray.php
@@ -5,7 +5,11 @@ namespace Ciebit\Documents\Builders;
 
 use Ciebit\Documents\Document;
 use Ciebit\Documents\Status;
+use Ciebit\Labels\Status as LabelStatus;
+use Ciebit\Files\Builders\Context as FileBuilder;
+use Ciebit\Labels\Label;
 use DateTime;
+use Exception;
 
 class FromArray implements Builder
 {
@@ -20,23 +24,30 @@ class FromArray implements Builder
     public function build(): Document
     {
         if (
-            ! is_array($this->data)
+            ! is_array($this->data) OR
+            ! isset($this->data['file']) OR
+            ! isset($this->data['label'])
         ) {
             throw new Exception('ciebit.documents.builders.invalid', 3);
         }
-        $file = (new StoryBuilder)->setData($this->data['story'])->build();
+        $label_data = $this->data['label'];
+        $file = (new FileBuilder)->setData($this->data['file'])->build();
+        $label = (new Label(
+            $label_data['title'],
+            $label_data['uri'],
+            new LabelStatus((int) $label_data['status'])
+        ))
+        ->setAscendantsId(array($label_data['ascendants_id']))
+        ->setId($label_data['id']);
         $status = $this->data['status'] ? new Status((int) $this->data['status']) : Status::DRAFT();
 
         $document = new Document(
             $file,
-            $status
+            $label,
+            new Status((int) $this->data['status'])
         );
         
-        $this->data['id'] && $document->setId((int) $this->data['id']);
-        $this->data['image'] && $document->setImage(
-           (new ImageBuilder)->setData($this->data['image'])->build()
-        );
-        
+        $this->data['id'] && $document->setId((int) $this->data['id']);        
 
         return $document;
     }
